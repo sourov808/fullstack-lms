8717a1b304766f48514e716802b0eb0b621d0dd3
sourov_das <sourovsd0009@gmail.com>
2026-02-24 21:28:56 +0600

    ready for deploy

--- test-font.js
+++
@@ -1,2 +0,0 @@
-const { Material_Symbols_Outlined } = require("next/font/google");
-console.log(typeof Material_Symbols_Outlined);
--- test-font.tsx
+++
@@ -1,2 +0,0 @@
-import { Material_Symbols_Outlined } from "next/font/google";
-console.log(Material_Symbols_Outlined);
